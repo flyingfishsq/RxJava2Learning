0aab8e611b4c777a2cfab3531e4d998cbbeb1423
Qi <flyingfishsq@aliyun.com>
2016-08-12 11:14:06 +0800

    the forth time commit

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@ public class Test{
 		System.out.println("This is my gitproject test, Hello world!");
 		System.out.println("The second time modify!");
 		System.out.println("The third time modify!");
+		System.out.println("The forth time modify!");
 	}
 	
 }
