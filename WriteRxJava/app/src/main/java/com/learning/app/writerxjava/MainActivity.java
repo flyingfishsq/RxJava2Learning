eb019b6adfea06d36e0e3a310cdab10c5d23e60a
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2020-04-01 10:54:55 +0800

    学习手写RxJava

---
+++ WriteRxJava/app/src/main/java/com/learning/app/writerxjava/MainActivity.java
@@ -0,0 +1,17 @@
+package com.learning.app.writerxjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 手写RxJava
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
