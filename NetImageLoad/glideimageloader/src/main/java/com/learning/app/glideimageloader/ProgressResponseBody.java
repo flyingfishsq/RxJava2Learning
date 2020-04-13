7985819b1badbf0586ceba6bf821ea71469bec4b
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2020-04-13 16:40:47 +0800

    非相关修改

--- NetImageLoad/glideimageloader/src/main/java/com/learning/app/glideimageloader/ProgressResponseBody.java
+++ NetImageLoad/glideimageloader/src/main/java/com/learning/app/glideimageloader/ProgressResponseBody.java
@@ -68,6 +68,6 @@ public class ProgressResponseBody extends ResponseBody {
                 super.close();
             }
         };
-        return source;
+        return forwardingSource;
     }
 }
