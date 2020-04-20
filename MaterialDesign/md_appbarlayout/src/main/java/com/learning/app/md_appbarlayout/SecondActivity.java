48fb86a810e89fecf6fb3f55d506f395fc9e9cd6
flyingfishsq <flyingfishsq@aliyun.com>
2020-04-21 07:26:11 +0800

    完成了使用Behavior实现Toolbar和FloatingActionButton的随着RecyclerView的拖动而显示和隐藏

--- MaterialDesign/md_appbarlayout/src/main/java/com/learning/app/md_appbarlayout/SecondActivity.java
+++ MaterialDesign/md_appbarlayout/src/main/java/com/learning/app/md_appbarlayout/SecondActivity.java
@@ -1,17 +1,64 @@
 package com.learning.app.md_appbarlayout;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.learning.app.md_appbarlayout.adapter.MyAdapter;
+import com.learning.app.md_appbarlayout.listener.FabScrollListener;
+import com.learning.app.md_appbarlayout.listener.HideShowListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * CoordinatorLayout继承自ViewGroup，通过协调并调度子控件或者布局来实现触摸（滑动）产生一些相关的动画效果
  */
 public class SecondActivity extends AppCompatActivity {
 
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.toolBar)
+    Toolbar toolBar;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    private List<String> mData = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
+
+        initData();
+        initView();
     }
+
+    private void initData() {
+        for (int i = 0; i < 20; i++) {
+            mData.add("这是第" + (i + 1) + "个条目");
+        }
+    }
+
+    private void initView() {
+        setSupportActionBar(toolBar);
+        setTitle("Behavior实现浮动按钮");
+        toolBar.setTitleTextColor(Color.YELLOW);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(mData));
+    }
+
 }
--- MaterialDesign/md_appbarlayout/src/main/java/com/learning/app/md_appbarlayout/behavior/MyFloatingButtonBehavior.java
+++ MaterialDesign/md_appbarlayout/src/main/java/com/learning/app/md_appbarlayout/behavior/MyFloatingButtonBehavior.java
@@ -3,18 +3,24 @@ package com.learning.app.md_appbarlayout.behavior;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.ViewCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.learning.app.md_appbarlayout.R;
 
 /**
  * 在xml里加上Behavior属性，是通过generateLayoutParams方法反射获取的
  */
 public class MyFloatingButtonBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
-    private boolean isAnimatingOut = true;
+    private boolean isAnimatingOut = false;
+    private Toolbar toolbar;
+    private CoordinatorLayout.LayoutParams layoutParams;
 
     public MyFloatingButtonBehavior(Context context, AttributeSet attr) {
         super(context, attr);
@@ -22,25 +28,36 @@ public class MyFloatingButtonBehavior extends CoordinatorLayout.Behavior<Floatin
 
     @Override
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        //当被观察的View（RecyclerView）滑动之前的回调
+        //滑动关联轴，super表示其它的由父类处理
+        layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+        toolbar = coordinatorLayout.findViewById(R.id.toolBar);
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
     }
 
     @Override
     public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type, @NonNull int[] consumed) {
-        if (dyConsumed > 0) {
-            animateOut(child);
-        } else if (dyConsumed < 0) {
-            animateIn(child);
+        //当被观察的View（RecyclerView）开始滑动的回调
+        if (dyConsumed > 20 && !isAnimatingOut) {
+            animateOut(child,toolbar);
+            isAnimatingOut = true;
+        } else if (dyConsumed < -20 && isAnimatingOut) {
+            animateIn(child,toolbar);
+            isAnimatingOut = false;
         }
 
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed);
     }
 
-    private void animateOut(FloatingActionButton view) {
-        ViewCompat.animate(view).translationY(0).start();
+    private void animateOut(FloatingActionButton view,Toolbar toolbar) {
+//        ViewCompat.animate(view).translationY(view.getHeight()+layoutParams.bottomMargin).start();
+        ViewCompat.animate(view).scaleX(0f).scaleY(0f).start();
+        ViewCompat.animate(toolbar).translationY(-toolbar.getHeight()).start();
     }
 
-    private void animateIn(FloatingActionButton view) {
-        ViewCompat.animate(view).translationY(view.getHeight()*2).start();
+    private void animateIn(FloatingActionButton view,Toolbar toolbar) {
+//        ViewCompat.animate(view).translationY(0).start();
+        ViewCompat.animate(view).scaleX(1f).scaleY(1f).start();
+        ViewCompat.animate(toolbar).translationY(0).start();
     }
 }
