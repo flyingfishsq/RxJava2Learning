b9d7bddc3043b75a9e9ccfa3164a713528dff866
flyingfishsq <flyingfishsq@aliyun.com>
2020-05-22 07:22:15 +0800

    增加注释

--- MaterialDesign/eventdelivery/src/main/java/com/learning/app/eventdelivery/view/MyCoordinatorLayout.java
+++ MaterialDesign/eventdelivery/src/main/java/com/learning/app/eventdelivery/view/MyCoordinatorLayout.java
@@ -9,6 +9,24 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import static com.learning.app.eventdelivery.MainActivity.TAG;
 
+/**
+ * ViewGroup中
+ *             if (actionMasked == MotionEvent.ACTION_DOWN) {
+ *                 // Throw away all previous state when starting a new touch gesture.
+ *                 // The framework may have dropped the up or cancel event for the previous gesture
+ *                 // due to an app switch, ANR, or some other state change.
+ *                 cancelAndClearTouchTargets(ev);
+ *                 resetTouchState();
+ *             }
+ *
+ * clearTouchTargets这个方法使mFirstTouchTarget = null;
+ *
+ * setDisallowIntercept
+ *
+ * onInterceptTouchEvent所有的ViewGroup都是默认不拦截的
+ *
+ * 所谓的拦截，是指按下去自身及以后的后续事件move，up等等
+ */
 public class MyCoordinatorLayout extends CoordinatorLayout {
     public MyCoordinatorLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
