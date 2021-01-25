046fede517904c52e0c78521969720bbbeb98f73
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2021-01-25 16:47:22 +0800

    使用全家桶进行网络请求

---
+++ chapter2_news_reader/src/main/java/com/example/chapter2_news_reader/pojo/VideoItem.java
@@ -0,0 +1,10 @@
+package com.example.chapter2_news_reader.pojo;
+
+/**
+ * Created by ttuo on 20/02/16.
+ */
+public class VideoItem extends ListItem {
+    public VideoItem(String title) {
+        super(title);
+    }
+}
