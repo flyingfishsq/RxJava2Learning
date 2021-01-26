d9e77bc24d30f14fb169e7ca5c302a4ed07a9e8e
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2021-01-26 16:29:02 +0800

    第七节学习

---
+++ chapter7_file_browser_3/src/androidTest/java/com/book/chapter7_file_browser_3/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.book.chapter7_file_browser_3;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.book.chapter7_file_browser_3", appContext.getPackageName());
+    }
+}
