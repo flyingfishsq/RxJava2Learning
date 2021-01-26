85f773c66cf03c295704c4f10cd0fbda1c4ae134
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2021-01-26 12:26:48 +0800

    第四节代码

---
+++ chapter4_flickr_client/src/main/java/com/book/chapter4_flickr_client/PhotoAdapter.java
@@ -0,0 +1,69 @@
+package com.book.chapter4_flickr_client;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
+import com.book.chapter4_flickr_client.pojo.Photo;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>{
+
+    final private Context context;
+    final private List<Photo> photos;
+
+    public PhotoAdapter(Context context, List<Photo> photos) {
+        this.context = context;
+        this.photos = photos;
+    }
+
+    @Override
+    public int getItemCount() {
+        return photos.size();
+    }
+
+    @Override
+    public PhotoViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.photo_list_card_view, viewGroup, false);
+        PhotoViewHolder viewHolder = new PhotoViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PhotoViewHolder photoViewHolder, int i) {
+        final Photo photo = photos.get(i);
+        photoViewHolder.personName.setText(photo.getTitle());
+        photoViewHolder.personAge.setText(photo.getUsername());
+        Picasso.with(context).load(photo.getThumbnailUrl())
+                .into(photoViewHolder.personPhoto);
+        //photoViewHolder.personPhoto.setImageResource(photos.get(i).photoId);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class PhotoViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        PhotoViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView) itemView.findViewById(R.id.cv);
+            personName = (TextView) itemView.findViewById(R.id.person_name);
+            personAge = (TextView) itemView.findViewById(R.id.person_age);
+            personPhoto = (ImageView) itemView.findViewById(R.id.person_photo);
+        }
+    }
+}
