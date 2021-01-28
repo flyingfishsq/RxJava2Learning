dad653c7fa67a3dcf6aa18bbb4680268e09df711
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2021-01-28 10:27:30 +0800

    第十三节

---
+++ chapter13_fan_animation/src/main/java/com/book/chapter13_fan_animation/FanItem.java
@@ -0,0 +1,13 @@
+package com.book.chapter13_fan_animation;
+
+public class FanItem {
+    private final String title;
+
+    public FanItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
